f5b56ec1d827836bae18434adf03c87e5d99aae1
Kristoffer Hansson <97549161+Zarser@users.noreply.github.com>
2022-04-11 21:32:03 +0200

    First version after 1.5h work
    Need to find the right calc for % over the amount and months for ea year

---
+++ index.js
@@ -0,0 +1,64 @@
+var slider = document.getElementById("slider-belopp");
+var output = document.getElementById("belopp");
+var inputbox = document.getElementById("värde");
+var slider1 = document.getElementById("slider-år");
+var output1 = document.getElementById("år");
+var inputbox1 = document.getElementById("år-längd");
+
+
+
+output.addEventListener = slider.value;
+
+slider.oninput = function () {
+    inputbox.value = this.value;
+}
+
+output1.addEventListener = slider1.value;
+
+slider1.oninput = function () {
+    inputbox1.value = this.value;
+}
+
+
+function calc() {
+    var belopp = parseInt(slider.value);
+    var år = parseInt(slider1.value);
+
+
+    if (belopp > 0 & år > 0);
+    document.getElementById("månads-kostnad").value = (Math.round(((belopp) / (år)) / 12 * 100) / 100);
+}
+
+document.getElementById('slider-belopp').onchange = function () {
+    console.log("Hej1");
+    calc();
+};
+
+document.getElementById('slider-år').onchange = function () {
+    console.log("Hej11");
+    calc();
+};
+
+
+
+
+
+slider.värde = function () {
+    inputbox.value = this.value;
+
+    calc();
+}
+
+
+
+slider1.värde = function () {
+    inputbox.value = this.value;
+
+    calc();
+}
+
+
+
+
+
+
